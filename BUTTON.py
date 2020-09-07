9390e3e0ac7af3610634a2a38825c455a8ee46dd
AndreeaConst <andreea.constantinescu2000@outlook.com>
2020-09-07 18:57:07 +0300

    Third commit

--- BUTTON.py
+++ BUTTON.py
@@ -9,10 +9,14 @@ class button():
         self.height = height
         self.text = text
 
-    def draw(self, screen, size, outline = None):
-        if outline:
-            pygame.draw.rect(screen, outline, (self.x-2, self.y-2, self.width+4, self.height+4), 0)
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height), 0)
+    def draw(self, screen, size, outline = None, shape = None):
+        if shape == None:
+            if outline:
+                pygame.draw.rect(screen, outline, (self.x-2, self.y-2, self.width+4, self.height+4), 0)
+            pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height), 0)
+        else:
+            points = [(350, 350), (400, 320), (460, 340), (410, 450)]
+            pygame.draw.polygon(screen, (0, 0, 255), points, 4)
 
         if self.text != '':
             font = pygame.font.SysFont('arial', size)
--- main.py
+++ main.py
@@ -10,24 +10,70 @@ town_view = pygame.image.load('images/town.png')
 townhall = pygame.image.load('images/townhall.png')
 recycling_area = pygame.image.load('images/recycling_area.jpg')
 mayor_recycling_area_background = pygame.image.load('images/mayor_recycling_area_background.jpg')
+recycling_factory = pygame.image.load('images/recycling_factory.jpg')
+
+#Objects
+arrow_right = pygame.image.load('arrow_right.png')
+arrow_down = pygame.image.load('arrow_down.png')
+arrow_up = pygame.image.load('arrow_up.png')
+arrow_left = pygame.image.load('arrow_left.png')
+
+bio_fuel = pygame.image.load('recycled items/organic/biofuel.png')
+compost = pygame.image.load('recycled items/organic/fertilizer.png')
+
+plastic_utensils = pygame.image.load('recycled items/plastic/plastic_utensils.png')
+bio_bag = pygame.image.load('recycled items/plastic/bio_bag.png')
+bottle = pygame.image.load('recycled items/plastic/bottle.jpg')
+cap = pygame.image.load('recycled items/plastic/cap.png')
+shoe = pygame.image.load('recycled items/plastic/shoe.png')
+schoolbag = pygame.image.load('recycled items/plastic/schoolbag.png')
+
+recycled_paper = pygame.image.load('recycled items/paper/paper.jpg')
+notebook = pygame.image.load('recycled items/paper/notebook.jpg')
+napkin = pygame.image.load('recycled items/paper/napkin.jpg')
+pencils = pygame.image.load('recycled items/paper/pencils.png')
+book = pygame.image.load('recycled items/paper/book.jpg')
+paper_cups = pygame.image.load('recycled items/paper/paper cups.jpg')
+
+plane = pygame.image.load('recycled items/metal/plane.png')
+car = pygame.image.load('recycled items/metal/car.jpg')
+lamp = pygame.image.load('recycled items/metal/lamp.jpg')
+bench = pygame.image.load('recycled items/metal/bench.jpg')
+chair = pygame.image.load('recycled items/metal/chair.jpg')
+coffemaker = pygame.image.load('recycled items/metal/coffeemaker.jpg')
+fridge = pygame.image.load('recycled items/metal/fridge.jpg')
+metal_utensils = pygame.image.load('recycled items/metal/metal utensils.jpg')
+microwave = pygame.image.load('recycled items/metal/microwave.jpg')
+oven = pygame.image.load('recycled items/metal/oven.jpg')
+toaster = pygame.image.load('recycled items/metal/toaster.jpg')
+
+glass_bottle = pygame.image.load('recycled items/glass/glass bottle.png')
+fiber_glass = pygame.image.load('recycled items/glass/fiberglass.jpg')
+glass_bricks = pygame.image.load('recycled items/glass/glass bricks.jpg')
+glassphalt = pygame.image.load('recycled items/glass/glassphalt.jpg')
+reflective_paint = pygame.image.load('recycled items/glass/reflective paint.png')
+tiles = pygame.image.load('recycled items/glass/tiles.jpg')
 
 #Characters
 mayor_normal_pose = pygame.image.load('poses/mayor_normal_pose.png')
 mayor_normal_poseBig = pygame.image.load('poses/mayor_normal_pose(big).png')
 mayor_superhero_pose = pygame.image.load('poses/mayor_superhero_pose.png')
+worker = pygame.image.load('poses/worker.png')
 
 #soundtrack
 menu_music = mixer.Sound('music/menu.wav')
 town_music = mixer.Sound('music/town.wav')
-areas_music = mixer.Sound('music/recycling_area.wav')
+recycling_area_music = mixer.Sound('music/recycling_area.wav')
+recycling_factory_music = mixer.Sound('music/recycling_factory.wav')
 
 # Title and Icon
 pygame.display.set_caption("Green Landia")
 icon = pygame.image.load('images/icon.png')
 pygame.display.set_icon(icon)
 
+#Others
 menuButton = BUTTON.button((255, 255, 255), 300, 180, 300, 70, 'START GAME')
-
+scorePlastic, scoreOrganic, scoreMetal, scorePaper, scoreGlass = 0, 0, 0, 0, 0
 
 #FUNCTIONS-----------------------------------------------------------------------------
 def drawButton(screen, BUTTON, size, outline):
@@ -94,7 +140,7 @@ def visit_recycling_area(screen):
     score, mistakes, oldmx, oldmy, x = 0, 0, 0, 0, 1000
     play, running = False, True
     while running:
-        areas_music.play(-1)
+        recycling_area_music.play(-1)
         screen.blit(mayor_recycling_area_background, (790,0))
         screen.blit(recycling_area, (0, 0))
 
@@ -155,6 +201,8 @@ def visit_recycling_area(screen):
                                     if mx >= 35 and mx <= 130:
                                         if material == 'metal':
                                             score += 1
+                                            global scoreMetal
+                                            scoreMetal += 1
                                             result = 'Correct'
 
                                             # recycle another piece of trash
@@ -168,6 +216,8 @@ def visit_recycling_area(screen):
                                         if mx >= 180 and mx <= 270:
                                             if material == 'organic':
                                                 score += 1
+                                                global scoreOrganic
+                                                scoreOrganic += 1
                                                 result = 'Correct'
 
                                                 # recycle another piece of trash
@@ -181,6 +231,8 @@ def visit_recycling_area(screen):
                                             if mx >= 340 and mx <= 415:
                                                 if material == 'paper':
                                                     score += 1
+                                                    global scorePaper
+                                                    scorePaper += 1
                                                     result = 'Correct'
 
                                                     # recycle another piece of trash
@@ -194,6 +246,8 @@ def visit_recycling_area(screen):
                                                 if mx >= 485 and mx <= 580:
                                                     if material == 'plastic':
                                                         score += 1
+                                                        global scorePlastic
+                                                        scorePlastic += 1
                                                         result = 'Correct'
 
                                                         # recycle another piece of trash
@@ -207,6 +261,8 @@ def visit_recycling_area(screen):
                                                     if mx >= 640 and mx <= 720:
                                                         if material == 'glass':
                                                             score += 1
+                                                            global scoreGlass
+                                                            scoreGlass += 1
                                                             result = 'Correct'
 
                                                             #recycle another piece of trash
@@ -261,9 +317,316 @@ def visit_recycling_area(screen):
 
         pygame.display.update()
 
-    areas_music.stop()
+    recycling_area_music.stop()
     town_music.play(-1)
     screen = pygame.display.set_mode((1300, 700))
+
+
+def tableOfContents_recyclingFactory(screen):
+    text = "   Hello! I am Paul, the chef worker here.\n   In the box to the left, you have the " \
+           " amount of\nrecycled materials of each kind so far and how much\nmaterial we need to recreate something else." \
+           "\n   We will create new products and put them\non the market." \
+           " You choose which products\nwe can recreate by clicking on them." \
+           "\n\n  CONSUME, RECYCLE, REUSE."
+    font = pygame.font.SysFont('arial', 20)
+    font1 = pygame.font.SysFont('arial', 15)
+    font2 = pygame.font.SysFont('arialblack', 30)
+
+    text_button = BUTTON.button((255, 255, 255), 800, 50, 400, 250, ' ')
+    table = BUTTON.button((0, 0, 0), 50, 50, 590, 630, ' ')
+    glass_square = BUTTON.button((0, 180, 0), 50, 50, 120, 130, ' ')
+    metal_square = BUTTON.button((0, 0, 255), 170, 50, 115, 130, ' ')
+    organic_square = BUTTON.button((255, 200, 0), 285, 50, 115, 130, ' ')
+    paper_square = BUTTON.button((255, 255, 0), 400, 50, 120, 130, ' ')
+    plastic_square = BUTTON.button((255, 0, 0), 520, 50, 120, 130, ' ')
+
+    glass = pygame.image.load('trash/trash1.png')
+    metal = pygame.image.load('trash/trash12.png')
+    organic = pygame.image.load('trash/trash36.png')
+    paper = pygame.image.load('trash/trash42.png')
+    plastic = pygame.image.load('trash/trash67.png')
+
+    drawButton(screen, text_button, 13, (0, 0, 0))  # the message of the worker
+    blit_text(screen, text, (802, 50), font)
+    drawButton(screen, table, 13, (0, 0, 0))
+
+    drawButton(screen, glass_square, 13, (0, 0, 0))
+    drawButton(screen, metal_square, 13, (0, 0, 0))
+    drawButton(screen, organic_square, 13, (0, 0, 0))
+    drawButton(screen, paper_square, 13, (0, 0, 0))
+    drawButton(screen, plastic_square, 13, (0, 0, 0))
+
+    screen.blit(glass, (43, 30))
+    score = font1.render("Glass: " + str(scoreGlass), True, (0, 0, 0))
+    screen.blit(score, (80, 150))
+
+    screen.blit(metal, (190, 50))
+    score = font1.render("Metal: " + str(scoreMetal), True, (0, 0, 0))
+    screen.blit(score, (195, 150))
+
+    screen.blit(organic, (307, 95))
+    score = font1.render("Organic: " + str(scoreOrganic), True, (0, 0, 0))
+    screen.blit(score, (315, 150))
+
+    screen.blit(paper, (395, 60))
+    score = font1.render("Paper: " + str(scorePaper), True, (0, 0, 0))
+    screen.blit(score, (430, 150))
+
+    screen.blit(plastic, (510, 70))
+    score = font1.render("Plastic: " + str(scorePlastic), True, (0, 0, 0))
+    screen.blit(score, (555, 150))
+
+    blit_text(screen, "BUILD ITEMS", (223, 180), font2, (255, 255, 255))
+
+def moreProductsRight_recyclingFactory(screen, worker):
+
+    while True:
+        font3 = pygame.font.SysFont('arialblack', 12)
+
+        tableOfContents_recyclingFactory(screen)
+
+        screen.blit(arrow_left, (50, 422))
+        screen.blit(worker, (700, 0))
+
+        z = 250
+        const = 2
+        for i in range(0, 3):
+            y = 80
+            if i == 1:
+                const += 1
+            for j in range(0, const):
+                white_square = BUTTON.button((255, 255, 255), y, z, 175, 130, ' ')
+                drawButton(screen, white_square, 13, (0, 0, 0))
+                y += 175
+            z += 130
+
+        screen.blit(bio_fuel, (130, 255))
+        blit_text(screen, "        BIOFUEL\n300 ORGANIC WASTE", (92, 344), font3, (0, 0, 0))
+
+        screen.blit(compost, (300, 247))
+        blit_text(screen, "       COMPOST\n50 ORGANIC WASTE", (270, 344), font3, (0, 0, 0))
+
+        screen.blit(cap, (100, 370))
+        blit_text(screen, "         CAP\n30 PLASTIC WASTE", (100, 475), font3, (0, 0, 0))
+
+        screen.blit(shoe, (280, 370))
+        blit_text(screen, "         SHOE\n50 PLASTIC WASTE", (275, 475), font3, (0, 0, 0))
+
+        screen.blit(schoolbag, (465, 375))
+        blit_text(screen, "      SCHOOLBAG\n 70 PLASTIC WASTE", (447, 475), font3, (0, 0, 0))
+
+        screen.blit(pencils, (120, 495))
+        blit_text(screen, "RECYCLED PAPER PENCIL\n     30 PAPER WASTE", (80, 607), font3, (0, 0, 0))
+
+        screen.blit(book, (280, 510))
+        blit_text(screen, "        BOOK\n80 PAPER WASTE", (280, 607), font3, (0, 0, 0))
+
+        screen.blit(paper_cups, (470, 510))
+        blit_text(screen, "   PAPER CUPS\n20 PAPER WASTE", (460, 607), font3, (0, 0, 0))
+
+        mx, my  = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if mx >= 40 and mx <= 70 and my >= 420 and my <= 450:
+                running = True
+                return running
+            if event.type == pygame.QUIT:
+                running = False
+                return running
+
+        pygame.display.update()
+
+def moreProductsRight1_recyclingFactory(screen, worker):
+
+    while True:
+        font3 = pygame.font.SysFont('arialblack', 12)
+
+        tableOfContents_recyclingFactory(screen)
+        screen.blit(arrow_left, (50, 422))
+        screen.blit(worker, (700, 0))
+
+        z = 250
+        const = 3
+        for i in range(0, 3):
+            if i == 2:
+                const -= 1
+            y = 80
+            for j in range(0, const):
+                white_square = BUTTON.button((255, 255, 255), y, z, 175, 130, ' ')
+                drawButton(screen, white_square, 13, (0, 0, 0))
+                y += 175
+            z += 130
+
+        screen.blit(glassphalt, (123, 250))
+        blit_text(screen, "    GLASSPHALT\n50 GLASS WASTE", (107, 345), font3, (0, 0, 0))
+
+        screen.blit(reflective_paint, (293, 250))
+        blit_text(screen, "REFLECTIVE PAINT\n 20 GLASS WASTE", (280, 345), font3, (0, 0, 0))
+
+        screen.blit(tiles, (467, 250))
+        blit_text(screen, "        TILES\n50 GLASS WASTE", (460, 345), font3, (0, 0, 0))
+
+        screen.blit(fridge, (120, 380))
+        blit_text(screen, "      FRIDGE\n90 METAL WASTE", (110, 474), font3, (0, 0, 0))
+
+        screen.blit(metal_utensils, (277, 380))
+        blit_text(screen, "METAL UTENSILS\n20 METAL WASTE", (280, 474), font3, (0, 0, 0))
+
+        screen.blit(microwave, (465, 380))
+        blit_text(screen, "    MICROWAVE\n70 METAL WASTE", (460, 474), font3, (0, 0, 0))
+
+        screen.blit(oven, (116, 510))
+        blit_text(screen, "        OVEN\n90 METAL WASTE", (105, 605), font3, (0, 0, 0))
+
+        screen.blit(toaster, (283, 510))
+        blit_text(screen, "      TOASTER\n40 METAL WASTE", (280, 605), font3, (0, 0, 0))
+
+        mx, my = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if mx >= 40 and mx <= 70 and my >= 420 and my <= 450:
+                running = True
+                return running
+            if event.type == pygame.QUIT:
+                running = False
+                return running
+
+        pygame.display.update()
+
+
+def moreProductsDown_recyclingFactory(screen, worker):
+
+    while True:
+        font3 = pygame.font.SysFont('arialblack', 12)
+
+        tableOfContents_recyclingFactory(screen)
+        screen.blit(worker, (700, 0))
+
+        z = 250
+        for i in range(0, 3):
+            y = 80
+            for j in range(0, 3):
+                white_square = BUTTON.button((255, 255, 255), y, z, 175, 130, ' ')
+                drawButton(screen, white_square, 13, (0, 0, 0))
+                y += 175
+            z += 130
+
+        screen.blit(glass_bottle, (120, 247))
+        blit_text(screen, "      BOTTLE \n20 GLASS WASTE", (110, 345), font3, (0, 0, 0))
+
+        screen.blit(fiber_glass, (290, 250))
+        blit_text(screen, "   FIBER GLASS\n20 GLASS WASTE", (280, 345), font3, (0, 0, 0))
+
+        screen.blit(glass_bricks, (470, 250))
+        blit_text(screen, "  GLASS BRICKS\n20 GLASS WASTE", (460, 345), font3, (0, 0, 0))
+
+        screen.blit(bench, (120, 380))
+        blit_text(screen, "       BENCH\n60 METAL WASTE", (100, 474), font3, (0, 0, 0))
+
+        screen.blit(chair, (295, 380))
+        blit_text(screen, "       CHAIR\n40 METAL WASTE", (285, 474), font3, (0, 0, 0))
+
+        screen.blit(coffemaker, (480, 380))
+        blit_text(screen, " COFFEE MAKER\n50 METAL WASTE", (460, 474), font3, (0, 0, 0))
+
+        screen.blit(plane, (100, 525))
+        blit_text(screen, "       PLANE\n300 METAL WASTE", (100, 605), font3, (0, 0, 0))
+
+        screen.blit(car, (280, 510))
+        blit_text(screen, "          CAR\n200 METAL WASTE", (275, 605), font3, (0, 0, 0))
+
+        screen.blit(lamp, (465, 510))
+        blit_text(screen, "         LAMP\n 20 METAL WASTE", (450, 605), font3, (0, 0, 0))
+
+
+        screen.blit(arrow_up, (320, 220))
+        screen.blit(arrow_right, (610, 433))
+
+        mx, my  = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if mx >= 315 and mx <= 340 and my >= 220 and my <= 240:
+                running = True
+                return running
+            if mx >= 605 and mx <= 625 and my >= 433 and my <= 463:
+                running = True
+                running = moreProductsRight1_recyclingFactory(screen, worker)
+                if running == False:
+                    return running
+            if event.type == pygame.QUIT:
+                running = False
+                return running
+
+        pygame.display.update()
+
+
+def visit_recycling_factory(screen):
+    font3 = pygame.font.SysFont('arialblack', 12)
+
+    running = True
+    x = 1000
+    while running:
+        recycling_factory_music.play(-1)
+        screen.blit(recycling_factory, (0, 0))
+
+        if(x > 700):
+            x -= 3
+        else:
+            tableOfContents_recyclingFactory(screen)
+
+            z = 250
+            const = 2
+            for i in range(0, 3):
+                y = 80
+                if i == 1:
+                    const += 1
+                for j in range(0, const):
+                    white_square = BUTTON.button((255, 255, 255), y, z, 175, 130, ' ')
+                    drawButton(screen, white_square, 13, (0, 0, 0))
+                    y += 175
+                z += 130
+
+            screen.blit(bio_fuel, (130, 255))
+            blit_text(screen, "        BIOFUEL\n300 ORGANIC WASTE", (92, 344), font3, (0, 0, 0))
+
+            screen.blit(compost, (300, 247))
+            blit_text(screen, "       COMPOST\n50 ORGANIC WASTE", (270, 344), font3, (0, 0, 0))
+
+            screen.blit(plastic_utensils, (100, 370))
+            blit_text(screen, "    BIO UTENSILS\n50 PLASTIC WASTE", (100, 475), font3, (0, 0, 0))
+
+            screen.blit(bio_bag, (280, 370))
+            blit_text(screen, "       BIO BAG\n20 PLASTIC WASTE", (275, 475), font3, (0, 0, 0))
+
+            screen.blit(bottle, (485, 380))
+            blit_text(screen, "BIO PLASTIC BOTTLE\n 10 PLASTIC WASTE", (447, 475), font3, (0, 0, 0))
+
+            screen.blit(recycled_paper, (125, 520))
+            blit_text(screen, "       PAPER STOCK\n     50 PAPER WASTE", (85, 607), font3, (0, 0, 0))
+
+            screen.blit(notebook, (280, 510))
+            blit_text(screen, "PAPER COVER NOTEBOOK\n     30 PAPER WASTE", (254, 607), font3, (0, 0, 0))
+
+            screen.blit(napkin, (445, 510))
+            blit_text(screen, "        PAPER NAPKIN\n      10 PAPER WASTE", (430, 607), font3, (0, 0, 0))
+
+            screen.blit(arrow_right, (610, 422))
+            screen.blit(arrow_down, (320, 645))
+
+            mx, my = pygame.mouse.get_pos()
+            if mx >= 600 and mx <= 640 and my >= 422 and my <= 440:
+                running = moreProductsRight_recyclingFactory(screen, worker)
+            if mx >= 315 and mx <= 345 and my >= 640 and my <= 670:
+                running = moreProductsDown_recyclingFactory(screen, worker)
+
+        screen.blit(worker, (x, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
+        pygame.display.update()
+
+    recycling_factory_music.stop()
+    town_music.play(-1)
 
 def townBackgrounds_Buttons(screen, recycling_area, supermarket, water_plant, recycling_plant,
                            business_center, touristic_area, eco_events, x):
@@ -475,10 +838,10 @@ def townView(screen):
                 else:
                     visit_button.color = (150, 240, 0)
 
-            # if event.type == pygame.MOUSEBUTTONDOWN:
-            #     if visit_button.isOver(pos):
-            #         town_music.stop()
-            #         visit_recycling_factory(screen)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if visit_button.isOver(pos):
+                    town_music.stop()
+                    visit_recycling_factory(screen)
 
         elif clickedButton == 5:
             screen.fill((0, 0, 0))
